7ed0e1276343b191e345aa9a21deeeba90c89d8d
object05 <alan.hablak@gmail.com>
2020-08-13 22:09:20 +0200

    Mower sound system working

--- Assets/Scripts/system/mowerSoundSystem.cs
+++ Assets/Scripts/system/mowerSoundSystem.cs
@@ -8,89 +8,45 @@ public class mowerSoundSystem : MonoBehaviour
     public AudioClip mowerRun;
     public AudioClip mowerStop;
 
-    AudioSource start;
-    AudioSource run;
-    AudioSource stop;
-
+    AudioSource src;
     MowerComponent mower;
 
     void Awake()
     {
         mower = gameObject.GetComponent<MowerComponent>();
-        if(mower == null)
-        {
-            Debug.Log("wtf");
-        }
+        src = gameObject.GetComponent<AudioSource>();
     }
 
-    void Start()
-    {
-        start = new AudioSource();
-        run = new AudioSource();
-        stop = new AudioSource();
-
-
-        if (start == null)
-        {
-            Debug.LogError("start == null");
-        }
-        if (run == null)
-        {
-            Debug.LogError("run == null");
-        }
-        if (stop == null)
-        {
-            Debug.LogError("stop == null");
-        }
-        start.clip = mowerStart;
-        run.clip = mowerRun;
-        run.loop = true;
-        stop.clip = mowerStop;
-    }
 
     void FixedUpdate()
     {
         switch (mower.state)
         {
             case MowerComponent.State.RUNNING:
-                if (!run.isPlaying)
+                if (!src.isPlaying)
                 {
-                    run.Play();
+                    src.loop = true;
+                    src.PlayOneShot(mowerRun);
                 }
                 break;
             case MowerComponent.State.TURN_OFF:
-                if (!stop.isPlaying)
-                {
-                    turnOffRunning();
-                    stop.Play();
-                    mower.state = MowerComponent.State.OFF;
-                }
+                src.Stop();
+                src.loop = false;
+                src.PlayOneShot(mowerStop);
+                mower.state = MowerComponent.State.OFF;
                 break;
             case MowerComponent.State.TURN_ON:
-                if (!start.isPlaying)
-                {
-                    start.Play();
-                    mower.state = MowerComponent.State.RUNNING;
-                }
+                src.Stop();
+                src.loop = false;
+                src.PlayOneShot(mowerStart);
+                mower.state = MowerComponent.State.RUNNING;
                 break;
             default:
                 break;
         }
     }
 
-    void turnOffRunning()
-    {
-        if (start.isPlaying)
-        {
-            start.Stop();
-        }
-        if (run.isPlaying)
-        {
-            run.Stop();
-        }
-        if (stop.isPlaying)
-        {
-            stop.Stop();
-        }
-    }
+
+
+
 }
